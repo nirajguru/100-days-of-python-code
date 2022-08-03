333255ca74ba4e48a315a9f7d9cabde65c82f314
Niraj Gurupanchayan <niraj.guru@gmail.com>
2022-08-03 23:50:22 +0100

    added flask app

---
+++ src/day11_guess_the_number_game.py
@@ -0,0 +1,22 @@
+import random
+
+level = input("Enter level (easy or hard): ").upper()
+
+if level == "EASY":
+    attempts = 10
+elif level == "HARD":
+    attempts = 5
+else:
+    print("Invalid level")
+
+answer_to_game = random.randint(1, 4)
+print(answer_to_game)
+guessed_number = input("Guess a number: ")
+
+while attempts > 0:
+    if answer_to_game == guessed_number:
+        print(f"You won! Guessed number is {answer_to_game}")
+        break
+    else:
+        guessed_number = int(input("Guess the number again: "))
+        attempts -=1
